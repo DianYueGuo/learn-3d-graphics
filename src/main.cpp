41252f88bcbb38ac40ddaae72d5c4e312f592953
DianYueGuo <dianyueguo@gmail.com>
2025-11-15 05:20:50 +0800

    fix Window or OpenGL context creation failure

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
     if (window) {
