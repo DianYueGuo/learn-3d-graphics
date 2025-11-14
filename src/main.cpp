31beafad0f8b73aa1e93c873bdb40f843a5c3987
DianYueGuo <dianyueguo@gmail.com>
2025-11-15 04:38:51 +0800

    add glfwInit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-#include <vector>
-
-#include "utility.hpp"
-#include "obj.hpp"
+#include <GLFW/glfw3.h>
 
 
 int main()
 {
-    std::vector<Vector3D> vertex_geometries;
-    read_obj_file("cube-Body.obj", vertex_geometries);
+    if (glfwInit()) {
+        std::cout << "GLFW initialization successed." << std::endl;
+    } else {
+        std::cout << "GLFW initialization failed." << std::endl;
+    }
 
     return 0;
 }
