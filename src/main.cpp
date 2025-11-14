e9191124f34d3aed5ded73b605ab2ea9209a87a0
DianYueGuo <dianyueguo@gmail.com>
2025-11-15 06:03:28 +0800

    free up if-else nesting

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@
 
 int main()
 {
-    if (glfwInit()) {
-        std::cout << "GLFW initialization succeeded." << std::endl;
-    } else {
+    if (!glfwInit()) {
         std::cout << "GLFW initialization failed." << std::endl;
         return EXIT_FAILURE;
     }
+    std::cout << "GLFW initialization succeeded." << std::endl;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -18,17 +17,16 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-    if (window) {
-        std::cout << "Window or OpenGL context creation succeeded." << std::endl;
-
-        while (!glfwWindowShouldClose(window)) {
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
-    } else {
+    if (!window) {
         std::cout << "Window or OpenGL context creation failed." << std::endl;
         glfwTerminate();
         return EXIT_FAILURE;
+    }
+    std::cout << "Window or OpenGL context creation succeeded." << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
